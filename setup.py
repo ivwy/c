9c376168e084946c2bc1a4c4debde9280431ac2f
ٴ <104566003+ivwy@users.noreply.github.com>
2022-10-26 09:29:55 +0900

    e.g. setup fore style

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@
 
 import discord, sys, os, platform
 from discord.ext import commands
+from colorama import Fore, Style
 from settings import token, prefix
-
-__author__ = "@ivwy (3468#3468)"
-__version__ = "v1.1.0"
 
 intents = discord.Intents.all()
 intents.members = True
+
 client = commands.Bot(command_prefix = prefix, help_command = None, intents = intents)
+
+__author__ = "@ivwy (3468#3468)"
+__version__ = "v1.1.0"
 
 def banner():
     sys.stdout.buffer.write(f'''
@@ -29,10 +31,11 @@ d8'  ,8P"88"  88
 @client.event
 async def on_ready():
     banner()
-    print(f"[INFO][SYSTEM]: You have logged in as {client.user.name}.")
+    print((Fore.GREEN + Style.BRIGHT) + f"[INFO][SYSTEM]: You have logged in as {client.user.name}.")
     print(f"[INFO][SYSTEM]: With ID {client.user.id}.")
     print(f"[INFO][SYSTEM]: Total {len(client.guilds)} servers connected.")
-    print(f"[INFO][SYSTEM]: Running on {platform.system()} {platform.release()} ({os.name})")
+    print(f"[INFO][SYSTEM]: Running on {platform.system()} {platform.release()} ({os.name}).")
+    print(f"[INFO][SYSTEM]: OS Information {platform.platform()}.")
     print(f"[INFO][SYSTEM]: API version {discord.__version__}.")
     print(f"[INFO][SYSTEM]: Python version {platform.python_version()}.\n")
 
@@ -40,19 +43,19 @@ async def on_ready():
     status = discord.Status.idle,
     activity = discord.Activity(
         type = discord.ActivityType.streaming,
-        name = f"in {len(client.guilds)} servers. | {prefix}help"))
+        name = f"in {len(client.guilds)} servers. || {prefix}help"))
 
 for file in os.listdir(f"./cogs"):
     if file.endswith(".py"):
         extension = file[:-3]
         try:
             client.load_extension(f"cogs.{extension}")
-            print(f"[INFO][SYSTEM]: Succesfully loaded {extension}.")
+            print((Fore.GREEN + Style.BRIGHT) + f"[INFO][SYSTEM]: Succesfully loaded {extension}.")
         except Exception as e:
             exception = f"{type(e).__name__}: {e}"
-            print(f"[INFO][SYSTEM]: Failed to load extension {extension}\n{exception}")
+            print((Fore.RED + Style.BRIGHT) + f"[INFO][SYSTEM]: Failed to load extension {extension}\n{exception}.")
 
 try:
     client.run(token)
 except Exception as error:
-    print(f"[INFO][SYSTEM]: Error when logging in: {error}.")
+    print((Fore.RED + Style.BRIGHT) + f"[INFO][SYSTEM]: Error when logging in: {error}.")
